2adf57bb684b92148f18b19ebfd74fd5193092ce
takeshid <goriponikeike55@gmail.com>
2025-12-28 15:59:34 +0900

    feat: add term theme option

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use clap::{
-    Args, CommandFactory, FromArgMatches, Parser, Subcommand,
+    Args, CommandFactory, FromArgMatches, Parser, Subcommand, ValueEnum,
     builder::{Styles, styling::AnsiColor},
 };
 use std::{io::IsTerminal, path::PathBuf};
@@ -19,18 +19,41 @@ pub struct Cli {
 #[derive(Debug, Subcommand)]
 pub enum Commands {
     Serve(FileArg),
-    Term(FileArg),
+    Term(TermArg),
 }
 
 #[derive(Debug, Args)]
 pub struct FileArg {
-    #[arg(value_name = "FILE")]
+    #[arg(value_name = "FILE", default_value = "README.md")]
     pub file: PathBuf,
+}
+
+#[derive(Debug, Args)]
+pub struct TermArg {
+    #[arg(value_name = "FILE", default_value = "README.md")]
+    pub file: PathBuf,
+    #[arg(long, value_enum, default_value = "glow")]
+    pub theme: ThemeChoice,
+}
+
+#[derive(Debug, Copy, Clone, ValueEnum)]
+pub enum ThemeChoice {
+    Glow,
+    Mono,
+    Catputtin,
+    Dracura,
+    Solarized,
+    Nord,
+    Ayu,
 }
 
 pub enum Mode {
     Serve { file: PathBuf, watch: bool },
-    Term { file: PathBuf, watch: bool },
+    Term {
+        file: PathBuf,
+        watch: bool,
+        theme: ThemeChoice,
+    },
 }
 
 impl Cli {
@@ -44,6 +67,10 @@ impl Cli {
         Self::from_arg_matches(&cmd.get_matches())
     }
     pub fn resolve_mode(self) -> Result<Mode> {
+        let root = match self.root {
+            Some(root) => root,
+            None => PathBuf::from("README.md"),
+        };
         match self.command {
             Some(Commands::Serve(arg)) => Ok(Mode::Serve {
                 file: arg.file,
@@ -52,12 +79,9 @@ impl Cli {
             Some(Commands::Term(arg)) => Ok(Mode::Term {
                 file: arg.file,
                 watch: self.watch,
+                theme: arg.theme,
             }),
             None => {
-                let root = match self.root {
-                    Some(root) => root,
-                    None => PathBuf::from("README.md"),
-                };
                 if std::io::stdout().is_terminal() {
                     Ok(Mode::Serve {
                         file: root,
@@ -67,6 +91,7 @@ impl Cli {
                     Ok(Mode::Term {
                         file: root,
                         watch: false,
+                        theme: ThemeChoice::Glow,
                     })
                 }
             }
