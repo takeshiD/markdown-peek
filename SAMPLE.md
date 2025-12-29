558fd6b0059f973173e4b7461672defdc7fa46cd
takeshid <goriponikeike55@gmail.com>
2025-12-29 11:58:19 +0900

    feat: implemented mathjax via cdn

--- SAMPLE.md
+++ SAMPLE.md
@@ -136,4 +136,5 @@ $$\left( \sum_{k=1}^n a_k b_k \right)^2 \leq \left( \sum_{k=1}^n a_k^2 \right) \
 ```mermaid
 graph TD
     A-->B
+    B-->C
 ```
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,33 @@
+function initializeMermaid() {
+    window.mermaid.initialize({ startOnLoad: false, isLight: "default" });
+    window.mermaid.run({ querySelector: "code.language-mermaid" });
+}
+
+function initializeMathJax() {
+    window.MathJax.typeset();
+}
+
 (function() {
-    window.mermaid.initialize({ startOnLoad: false, isLight: "default" })
-    window.mermaid.run({ querySelector: "code.language-mermaid" })
-})()
+    initializeMermaid();
+    initializeMathJax();
+    (async function() {
+        const socket = new WebSocket("http://127.0.0.1:3000/ws");
+        console.log("Create webscoket");
+        socket.onopen = function (event) {
+            console.log(`Open: ${event}`);
+        };
+        socket.onmessage = function (event) {
+            console.log(`Received: ${event}`);
+            if (event.data === "reload") {
+                socket.close();
+                window.location.reload();
+            }
+        };
+        socket.onerror = function (event) {
+            console.log(`Error: ${event}`);
+        };
+        socket.onclose = function (event) {
+            console.log(`Close: ${event}`);
+        };
+    })();
+}());
